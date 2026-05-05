f7500061de7c1b1265ed33802b9c1b442b1caeb4
ChauManh <chaumanh1108@gmail.com>
2026-05-05 19:37:33 +0700

    update(fe): fix image cdn url

--- fe-nextjs/next.config.ts
+++ fe-nextjs/next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
+  images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "order-food-media.s3.ap-southeast-1.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "d3j0s5326wg516.cloudfront.net",
         pathname: "/**",
       },
     ],
